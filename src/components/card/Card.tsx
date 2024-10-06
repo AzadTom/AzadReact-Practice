1260c77d635cbe82e6536db6f15faf67e23806f1
AzadTom <kumarazad2917@gmail.com>
2024-10-06 15:02:20 +0530

    practice basics testing through vitest and other library

---
+++ src/components/card/Card.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+interface Props{
+    name:string
+}
+
+function Card(props:Props) {
+  const { name } = props;  
+  return (
+    <div className='w-[100px] h-[100px] flex justify-center items-center'>
+        <h1>{name}</h1>
+    </div>
+  )
+}
+
+export default Card
