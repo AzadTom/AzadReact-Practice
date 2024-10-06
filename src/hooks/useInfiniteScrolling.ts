9be0916f023352029d4e59513631f7283e67f438
AzadTom <kumarazad2917@gmail.com>
2024-10-06 22:47:13 +0530

    implemented infinite scrolling in react

---
+++ src/hooks/useInfiniteScrolling.ts
@@ -0,0 +1,78 @@
+import { useEffect, useState, useCallback, RefObject } from "react";
+
+export interface ListType {
+  bloggerAvatar: string;
+  bloggerId: number;
+  bloggerName: string;
+  canViewBlog: boolean;
+  isProBlog: boolean;
+  is_bookmarked: boolean;
+  itemId: number;
+  meta_data: string;
+  primaryAgeGroup: string;
+  proText: string;
+  thumb: string;
+  title: string;
+  type: string;
+  url: string;
+  viewsCount: string;
+}
+
+const useInfiniteScrolling = (refs: RefObject<HTMLDivElement>) => {
+
+  const [page, setPage] = useState(1);
+  const [list, setList] = useState<ListType[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getList = useCallback(async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(
+        `https://api9.parentune.com/blogs/blogs?page=${page}`
+      );
+      const result = await response.json();
+      setList((prevList) => [...prevList, ...result.list]);
+
+      console.log("result:",result)
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [page]);
+
+
+  useEffect(()=>{
+    getList();
+  },[getList])
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: "20px",
+      threshold: 1.0,
+    };
+
+    const observer = new IntersectionObserver(([entry]) => {
+      if (entry.isIntersecting && !isLoading) {
+        setPage((prev) => prev + 1);
+      }
+    }, options);
+
+    if (refs.current) {
+      observer.observe(refs.current);
+    }
+
+    return () => {
+      if (refs.current) {
+        observer.unobserve(refs.current);
+      }
+    };
+  }, [refs,isLoading]);
+
+  return { isLoading, list };
+
+
+};
+
+export default useInfiniteScrolling;
