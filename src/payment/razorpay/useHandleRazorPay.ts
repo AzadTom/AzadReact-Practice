d2a8ef3db1c227dfec63b2bbc0fb1f51d94541f0
AzadTom <kumarazad2917@gmail.com>
2024-12-13 00:10:58 +0530

    comment-out-razorpay-page

--- src/payment/razorpay/useHandleRazorPay.ts
+++ src/payment/razorpay/useHandleRazorPay.ts
@@ -1,56 +1,56 @@
- /*script function */
- function loadScript(src: any) {
-    return new Promise((resolve) => {
-        const script = document.createElement('script');
-        script.src = src;
-        script.onload = () => {
-            resolve(true);
-        };
-        script.onerror = () => {
-            resolve(false);
-        };
-        document.body.appendChild(script);
-    });
-} 
+//  /*script function */
+//  function loadScript(src: any) {
+//     return new Promise((resolve) => {
+//         const script = document.createElement('script');
+//         script.src = src;
+//         script.onload = () => {
+//             resolve(true);
+//         };
+//         script.onerror = () => {
+//             resolve(false);
+//         };
+//         document.body.appendChild(script);
+//     });
+// } 
 
 
-// razorpay starts
-async function displayRazorpay() {
+// // razorpay starts
+// async function displayRazorpay() {
    
-    const res = await loadScript('https://checkout.razorpay.com/v1/checkout.js');
+//     const res = await loadScript('https://checkout.razorpay.com/v1/checkout.js');
 
-     /*if razorpay fails to load */
-        if (!res) {
-            alert('Razorpay SDK failed to load. Are you online?');
-            return;
-        }
+//      /*if razorpay fails to load */
+//         if (!res) {
+//             alert('Razorpay SDK failed to load. Are you online?');
+//             return;
+//         }
 
 
-        // first you get order id  then it is starts
+//         // first you get order id  then it is starts
 
-        const options = {
-            key: 'razorpay_key',
-            currency: '',
-            name: '',
-            description: 'Thank you',
-            subscription_id: '',
-            handler: async function (response: any) {
+//         const options = {
+//             key: 'razorpay_key',
+//             currency: '',
+//             name: '',
+//             description: 'Thank you',
+//             subscription_id: '',
+//             handler: async function (response: any) {
                
-            },
-            prefill: {
-                name: '',
-                email: '',
-                contact: ''
-            },
-            modal: {
-                ondismiss: function () {
+//             },
+//             prefill: {
+//                 name: '',
+//                 email: '',
+//                 contact: ''
+//             },
+//             modal: {
+//                 ondismiss: function () {
                    
-                }
-            }
-        };
+//                 }
+//             }
+//         };
 
-        const _window = window as any;
-        const paymentObject = new _window.Razorpay(options);
-        paymentObject.open();
+//         const _window = window as any;
+//         const paymentObject = new _window.Razorpay(options);
+//         paymentObject.open();
 
-}
+// }
