db8877613a7ba9a978a3d0421d4bccbd1d6465b1
AzadTom <kumarazad2917@gmail.com>
2024-12-15 12:32:35 +0530

    added hoverEffect to ShowWithCard

--- src/features/Utils/ShowWithCard.tsx
+++ src/features/Utils/ShowWithCard.tsx
@@ -1,22 +1,23 @@
+import { Fragment, useState } from "react";
+
 type TShowWithCardProps = {
   text: string;
-  index:number;
-  img:string;
+  index: number;
+  img: string;
   onClick: () => void;
 };
 
 const ShowWithCard = (props: TShowWithCardProps) => {
+  const [isHovered, setIsHovered] = useState<boolean>(false);
 
-  const { text, onClick,img,index} = props;
   return (
     <div
       className="cursor-pointer rounded-md bg-[#242424] border border-[#313131] hover:border-[#3c3c3c] overflow-hidden flex flex-col"
-      onClick={onClick}
+      onClick={props.onClick}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
     >
-      <div className="w-full h-full">
-        <img src={img} alt="saitam-img" loading="lazy" />
-      </div>
-      <p className="text-white text-sm uppercase px-4 py-2 text-center">{index}-{text}</p>
+      {isHovered ? <HoverContent {...props}/> : <Content {...props} />}
     </div>
   );
 };
@@ -24,3 +25,29 @@ const ShowWithCard = (props: TShowWithCardProps) => {
 export default ShowWithCard;
 
 
+const HoverContent = (props:TShowWithCardProps)=>{
+
+
+  return(
+    <div className="w-full h-full flex flex-col justify-center items-center">
+      <h2 className="bg-black rounded-full px-4 py-2 text-xl font-medium">{props.index}</h2>
+      <p className="text-xl font-medium">{props.text}</p>
+    </div>
+  )
+
+}
+
+
+const Content = (props: TShowWithCardProps) => {
+  const { img, text } = props;
+  return (
+    <Fragment>
+      <div className="w-full h-full">
+        <img src={img} alt="saitam-img" loading="lazy" />
+      </div>
+      <p className="text-white text-sm uppercase px-4 py-2 text-center">
+        {text}
+      </p>
+    </Fragment>
+  );
+};
