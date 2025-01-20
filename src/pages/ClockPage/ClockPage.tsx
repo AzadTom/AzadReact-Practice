1d3eaf5765ddf42cd47dbe3d596c45b2778851e3
AzadTom <kumarazad2917@gmail.com>
2025-01-20 23:45:48 +0530

    added clock to react practice project.

--- src/pages/ClockPage/ClockPage.tsx
+++ src/pages/ClockPage/ClockPage.tsx
@@ -1,10 +1,64 @@
+import React, { useEffect } from "react";
 import "./style.css";
+
+
+
+
 const ClockPage = () => {
+
+  const displayTime = () => {
+    const hr = document.getElementById("hour");
+    const min = document.getElementById("min");
+    const sec = document.getElementById("sec");
   
+    const date = new Date();
+    const hh = date.getHours();
+    const mm = date.getMinutes();
+    const ss = date.getSeconds();
+  
+    const hR = 30 * hh + mm / 2;
+    const mR = 6 * mm;
+    const sR = 6 * ss;
+  
+    if (hr && min && sec) {
+      hr.style.transform = `rotate(${hR}deg)`;
+      min.style.transform = `rotate(${mR}deg)`;
+      sec.style.transform = `rotate(${sR}deg)`;
+    }
+  };
+  
+  const setDays = () => {
+    const date0 = document.getElementById("date-0");
+    const date1 = document.getElementById("date-1");
+    const daySpan = document.getElementById("day");
+    const weekDays = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+  
+    const date = new Date();
+    const dt = date.getDate();
+    const day = date.getDay();
+    const paddedDate = dt.toString().padStart(2, "0");
+  
+    if (date0 && date1 && daySpan) {
+      date0.innerText = paddedDate[0];
+      date1.innerText = paddedDate[1];
+      daySpan.innerText = weekDays[day];
+    }
+  };
+
+  
+
+  useEffect(() => {
+    setInterval(() => {
+      displayTime();
+    }, 1000);
+    setDays();
+  }, []);
+
   return (
-    <section className="home-page">
+    <section>
       <div className="container">
         <div className="clock">
+          {/* hour,minute,second */}
           <div className="hand" id="hour">
             <i></i>
           </div>
@@ -14,44 +68,18 @@ const ClockPage = () => {
           <div className="hand" id="sec">
             <i></i>
           </div>
-
-          <span className="nums" style={{ transform: "rotate(30deg)" }}>
-            <b>1</b>
-          </span>
-          <span className="nums" style={{ transform: "rotate(60deg)" }}>
-            <b>2</b>
-          </span>
-          <span className="nums" style={{ transform: "rotate(90deg)" }}>
-            <b>3</b>
-          </span>
-          <span className="nums" style={{ transform: "rotate(120deg)" }}>
-            <b>4</b>
-          </span>
-          <span className="nums" style={{ transform: "rotate(150deg)" }}>
-            <b>5</b>
-          </span>
-          <span className="nums" style={{ transform: "rotate(180deg)" }}>
-            <b>6</b>
-          </span>
-          <span className="nums" style={{ transform: "rotate(210deg)" }}>
-            <b>7</b>
-          </span>
-          <span className="nums" style={{ transform: "rotate(240deg)" }}>
-            <b>8</b>
-          </span>
-          <span className="nums" style={{ transform: "rotate(270deg)" }}>
-            <b>9</b>
-          </span>
-          <span className="nums" style={{ transform: "rotate(300deg)" }}>
-            <b>10</b>
-          </span>
-          <span className="nums" style={{ transform: "rotate(330deg)" }}>
-            <b>11</b>
-          </span>
-          <span className="nums" style={{ transform: "rotate(360deg)" }}>
-            <b>12</b>
-          </span>
-
+          {/* hour,minute,second */}
+          {/* nums */}
+          {Array.from({ length: 12 }).map((_, index: number) => (
+            <span
+               key={index+1}
+              className="nums"
+              style={{ "--i": index + 1 } as React.CSSProperties}
+            >
+              <b>{index + 1}</b>
+            </span>
+          ))}
+          {/* nums */}
           <div className="date">
             <span id="date-0">0</span>
             <span id="date-1">0</span>
@@ -66,3 +94,4 @@ const ClockPage = () => {
 };
 
 export default ClockPage;
+
