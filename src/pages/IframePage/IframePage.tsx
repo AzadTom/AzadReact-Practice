f75d3f3aefdbe17748a73ee3997552bb00d8dfa7
AzadTom <kumarazad2917@gmail.com>
2025-01-20 19:06:23 +0530

    added https from http

--- src/pages/IframePage/IframePage.tsx
+++ src/pages/IframePage/IframePage.tsx
@@ -2,7 +2,7 @@ const IframePage = () => {
   return (
     <div className="bg-white" style={{display:"flex", justifyContent:"center",alignContent:"end", height:"100vh"}}>
       <iframe
-        src="http://34.47.153.39/iframe/daily-hunt/ovulation-calculator"
+        src="https://34.47.153.39/iframe/daily-hunt/ovulation-calculator"
         width="360px"
         height="500px"
       ></iframe>
