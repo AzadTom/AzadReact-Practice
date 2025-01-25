a350e2ddc37bb41f688e375ecd34e90f3270fc9e
AzadTom <kumarazad2917@gmail.com>
2025-01-26 01:27:15 +0530

    added curve line to comment.

--- src/pages/ButtonPage/ButtonPage.tsx
+++ src/pages/ButtonPage/ButtonPage.tsx
@@ -7,19 +7,10 @@ const ButtonPage = () => {
     <Fragment>
       <GoBack />
       <section className="max-w-[1000px] mt-4 mx-auto flex flex-wrap gap-4 px-4">
-        {Array.from({ length: 50 }, (_, index: number) => {
-          return (
-            <Fragment key={index}>
-              <Button className="px-2.5 py-1 text-xs bg-[#006239] text-white rounded-md border border-[#0d7b4c] hover:bg-[#287150] hover:border-[#38b77e]">
-                Start your project
-              </Button>
-              <Button className="px-2.5 py-1 text-xs bg-[#242424] text-white rounded-md border border-[#313131] hover:border-[#3c3c3c]">
-                Sign in
-              </Button>
-            </Fragment>
-          );
-        })}
-        <ReButton className="bg-pink-600 border border-pink-900 hover:bg-pink-400 hover:border-pink-400 rounded-full">
+        <Button className="px-2.5 py-1 text-xs bg-[#006239] text-white rounded-full border border-[#0d7b4c] hover:bg-[#287150] hover:border-[#38b77e]">
+          Start your project
+        </Button>
+        <ReButton className="bg-pink-600 border border-pink-900 hover:bg-pink-400 hover:border-pink-400 text-[14px] font-medium rounded-full">
           Munna Bhaiya
         </ReButton>
       </section>
--- src/pages/NestedComment/NestedComment.tsx
+++ src/pages/NestedComment/NestedComment.tsx
@@ -6,6 +6,21 @@ function NestedComment() {
     <div>
       <GoBack />
       <CommentContainer />
+
+      <div className="max-w-[1000px] mx-auto p-4">
+        <div className="w-[2px] h-[100px] bg-white"></div>
+        <div className="w-[50px] h-[1px] bg-white"></div>
+      </div>
+
+      <div className="max-w-[1000px] mx-auto">
+        {/* <div
+          data-testid="branch-line"
+          className={`box-border h-md bg-white  border-0 border-tone-4 border-solid border-b-[1px] cursor-pointer w-[calc(50%+0.5px)] h-[10px]  border-l-[1px] rounded-bl-[12px]`}
+        /> */}
+
+         <div className="size-[50px]  m-4 border-b-4 border-l-4  border-white rounded-bl-[50px]">
+         </div>
+      </div>
     </div>
   );
 }
