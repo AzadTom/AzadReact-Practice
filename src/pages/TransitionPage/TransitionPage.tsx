d631d90b4ec1d1aa098850c1d3fa525d6b6770e2
AzadTom <kumarazad2917@gmail.com>
2025-02-04 11:48:10 +0530

    added transition property for words opacity.

--- src/pages/TransitionPage/TransitionPage.tsx
+++ src/pages/TransitionPage/TransitionPage.tsx
@@ -1,8 +1,8 @@
 import { Fragment } from "react/jsx-runtime";
 import "./styles.modules.css";
 import { useEffect, useRef } from "react";
-import { MotionValue, useScroll, useTransform,motion } from "motion/react";
-import Lenis from 'lenis'
+import { MotionValue, useScroll, useTransform, motion } from "motion/react";
+import Lenis from "lenis";
 import { useDimension } from "../../hooks/common/useDimension";
 
 const TransitionPage = () => {
@@ -17,6 +17,7 @@ const TransitionPage = () => {
         <Transition1 />
       </div>
       <GalleryScrollTransition />
+      <TextOpacityTransition />
     </section>
   );
 };
@@ -116,52 +117,171 @@ const GalleryTransition = () => {
 };
 
 const GalleryScrollTransition = () => {
+  const { height } = useDimension();
+  const container = useRef<HTMLDivElement | null>(null);
 
-  const { height} = useDimension();
-  const container = useRef<HTMLDivElement|null>(null);
-
-  const {scrollYProgress} = useScroll({
-    offset:["start end","end start"]
+  const { scrollYProgress } = useScroll({
+    offset: ["start end", "end start"],
   });
 
-  const  y1 = useTransform(scrollYProgress,[0,1],[0,height*2]);
-  const  y2 = useTransform(scrollYProgress,[0,1],[0,height*2.3]);
-  const  y3 = useTransform(scrollYProgress,[0,1],[0,height*3.3]);
-  const  y4 = useTransform(scrollYProgress,[0,1],[0,height*2.2]);
+  const y1 = useTransform(scrollYProgress, [0, 1], [0, height * 2]);
+  const y2 = useTransform(scrollYProgress, [0, 1], [0, height * 2.3]);
+  const y3 = useTransform(scrollYProgress, [0, 1], [0, height * 3.3]);
+  const y4 = useTransform(scrollYProgress, [0, 1], [0, height * 2.2]);
 
-  useEffect(()=>{
+  useEffect(() => {
     const lenis = new Lenis();
-    function raf(time:number) {
+    function raf(time: number) {
       lenis.raf(time);
       requestAnimationFrame(raf);
     }
     requestAnimationFrame(raf);
-  },[])
-
+  }, []);
 
   return (
     <main>
-       <div className="h-[100vh]"></div>
-      <div ref={container} className="gallery overflow-hidden bg-white p-4 rounded-2xl">
-        <Column images={["/gallery-1.jpg","/gallery-1.jpg","/gallery-1.jpg","/gallery-1.jpg"]} y={y1}/>
-        <Column images={["/gallery-1.jpg","/gallery-1.jpg","/gallery-1.jpg","/gallery-1.jpg"]} y={y2}/>
-        <Column images={["/gallery-1.jpg","/gallery-1.jpg","/gallery-1.jpg","/gallery-1.jpg"]} y={y3}/>
-        <Column images={["/gallery-1.jpg","/gallery-1.jpg","/gallery-1.jpg","/gallery-1.jpg"]} y={y4}/>
+      <div className="h-[100vh]"></div>
+      <div
+        ref={container}
+        className="gallery overflow-hidden bg-white p-4 rounded-2xl"
+      >
+        <Column
+          images={[
+            "/gallery-1.jpg",
+            "/gallery-1.jpg",
+            "/gallery-1.jpg",
+            "/gallery-1.jpg",
+          ]}
+          y={y1}
+        />
+        <Column
+          images={[
+            "/gallery-1.jpg",
+            "/gallery-1.jpg",
+            "/gallery-1.jpg",
+            "/gallery-1.jpg",
+          ]}
+          y={y2}
+        />
+        <Column
+          images={[
+            "/gallery-1.jpg",
+            "/gallery-1.jpg",
+            "/gallery-1.jpg",
+            "/gallery-1.jpg",
+          ]}
+          y={y3}
+        />
+        <Column
+          images={[
+            "/gallery-1.jpg",
+            "/gallery-1.jpg",
+            "/gallery-1.jpg",
+            "/gallery-1.jpg",
+          ]}
+          y={y4}
+        />
       </div>
       <div className="h-[100vh]"></div>
     </main>
   );
 };
 
-const Column = ({ images ,y}: { images: string[],y?:MotionValue<number> }) => {
-
+const Column = ({
+  images,
+  y,
+}: {
+  images: string[];
+  y?: MotionValue<number>;
+}) => {
   return (
-    <motion.div  style={{y}} className="column">
+    <motion.div style={{ y }} className="column">
       {images.map((src: string, index: number) => (
         <div key={index} className="w-full h-full">
-          <img src={src} alt={"thumnail"} className="min-w-[280px] w-full h-[600px] object-cover" />
+          <img
+            src={src}
+            alt={"thumnail"}
+            className="min-w-[280px] w-full h-[600px] object-cover"
+          />
         </div>
       ))}
     </motion.div>
+  );
+};
+
+const TextOpacityTransition = () => {
+  // const textRefs = useRef<HTMLParagraphElement | null>(null);
+  // const { scrollYProgress } = useScroll({
+  //   target: textRefs,
+  //   offset: ["start 0.9", "start 0.25"],
+  // });
+
+  const value = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+        quibusdam eius dignissimos doloremque, distinctio nostrum blanditiis
+        similique dolores quis quidem deserunt optio id, porro illum dolor earum
+        aperiam, provident quaerat eos. Cum, rerum quo?`;
+
+  return (
+    <div>
+      <div className="h-screen"></div>
+      {/* <motion.p ref={textRefs} className="text-[50px] max-w-[1280px] p-[40px]" style={{opacity:scrollYProgress}}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+        quibusdam eius dignissimos doloremque, distinctio nostrum blanditiis
+        similique dolores quis quidem deserunt optio id, porro illum dolor earum
+        aperiam, provident quaerat eos. Cum, rerum quo?{" "}
+      </motion.p> */}
+      <WordTransition value={value} />
+      <div className="h-screen"></div>
+    </div>
+  );
+};
+
+const WordTransition = ({ value }: { value: string }) => {
+  const textRefs = useRef<HTMLParagraphElement | null>(null);
+  const { scrollYProgress } = useScroll({
+    target: textRefs,
+    offset: ["start 0.9", "start 0.25"],
+  });
+
+  const words = value.split(" ");
+
+  return (
+    <p ref={textRefs} className="text-[32px] max-w-[1280px] p-[40px]">
+      {words.map((word: string, index: number) => {
+        const start = index / words.length;
+        const end = start + 1 / words.length;
+        return (
+          <Word
+            key={index}
+            value={word}
+            range={[start, end]}
+            progress={scrollYProgress}
+          />
+        );
+      })}
+    </p>
+  );
+};
+
+const Word = ({
+  value,
+  progress,
+  range,
+}: {
+  value: string;
+  range: number[];
+  progress: MotionValue<number>;
+}) => {
+  const opacity = useTransform(progress, range, [0, 1]);
+
+  return (
+    <span className="relative">
+      <span className="absolute inset-0" style={{ opacity: 0.1 }}>
+        {value}
+      </span>
+      <motion.span style={{ opacity: opacity }}>
+        {value}
+      </motion.span>
+    </span>
   );
 };
