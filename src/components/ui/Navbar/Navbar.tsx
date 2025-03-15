1e5bf53d3d18f39b0990daa63abbc5ca5f5370a1
AzadTom <kumarazad2917@gmail.com>
2025-03-15 21:18:31 +0530

    added chnages to files

--- src/components/ui/Navbar/Navbar.tsx
+++ src/components/ui/Navbar/Navbar.tsx
@@ -1,10 +1,13 @@
+import { useNavigate } from "react-router-dom";
 import { NavigationSubMenuTransition } from "../../../pages/TransitionPage/TransitionPage";
 import AnimatedNavigation from "../../AnimatedNavigation/AnimatedNavigation";
 
 const NavBar = () => {
+
+  const navigate = useNavigate();
   return (
     <nav className="sticky top-0 p-4 bg-pink-900 border border-pink-600 flex items-center justify-between">
-      <div className="max-w-[max-content] text-[18px] sm:text-[34px] font-bold">
+      <div className="max-w-[max-content] text-[18px] sm:text-[34px] font-bold cursor-pointer" onClick={()=> navigate("/")}>
         ReactJS Practice
       </div>
       <div className="z-[9999]">
